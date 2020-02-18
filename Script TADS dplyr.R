ae76ad373b769b9d0ce825d0594bde3c39176ff6
Paulo Ricardo <pr838908@gmail.com>
2020-02-18 11:52:56 -0300

    Código Mudado

--- Script TADS dplyr.R
+++ Script TADS dplyr.R
@@ -9,4 +9,5 @@ iris %>%  #ctr+shift+m= novo pipe
   select(Species) #seleciona apenas espécies com pétalas acima de 2
 
 mutate(iris, Petal.Length2=Petal.Length+2) #cria nova coluna depois da última com nome dado + informação inserida.
+#alterei aqui
 
